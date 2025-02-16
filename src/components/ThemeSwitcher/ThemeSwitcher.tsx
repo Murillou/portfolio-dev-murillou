44010f8f8f49463d936c231febf805377fbaeab7
Murillou <murillovinicius13@hotmail.com>
2025-02-16 18:20:02 -0300

    feat: adiciona componente ThemeSwitcher com funcionalidade

---
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -0,0 +1,15 @@
+import { useTheme } from '@/context/ThemeContext';
+import { RiMoonClearLine, RiSunLine } from 'react-icons/ri';
+
+export function ThemeSwitcher() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="ml-4 p-2 rounded-full bg-light-muted dark:bg-dark-muted text-light-text dark:text-dark-text transition-transform duration-300 ease-in-out transform hover:scale-110"
+    >
+      {theme === 'light' ? <RiMoonClearLine /> : <RiSunLine />}
+    </button>
+  );
+}

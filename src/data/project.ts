2552cc0fc6894ce5d4be3e074c2a9bddfdb57fab
Murillou <murillovinicius13@hotmail.com>
2025-02-16 20:30:56 -0300

    refactor: altera o caminho do diretório para as imagens

--- src/data/project.ts
+++ src/data/project.ts
@@ -1,6 +1,6 @@
-import coffeeDelivery from '../../assets/coffee-delivery.png';
-import todoList from '../../assets/todo-list.png';
-import nextShop from '../../assets/next-shop.png';
+import coffeeDelivery from '../assets/coffee-delivery.png';
+import todoList from '../assets/todo-list.png';
+import nextShop from '../assets/next-shop.png';
 
 export const projetos = [
   {

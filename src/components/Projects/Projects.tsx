a8fe3ff12b40f34d0710d615d9fc6294ff839857
Murillou <murillovinicius13@hotmail.com>
2025-02-16 20:17:44 -0300

    chore: refatora componentes para extrair dados para arquivos separados

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,7 +1,4 @@
 import Modal from 'react-modal';
-import coffeeDelivery from '../../assets/coffee-delivery.png';
-import todoList from '../../assets/todo-list.png';
-import nextShop from '../../assets/next-shop.png';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -10,6 +7,7 @@ import { IoMdCloseCircleOutline } from 'react-icons/io';
 import { useEffect } from 'react';
 import { useTheme } from '@/context/ThemeContext';
 import { CardProject } from './CardProject';
+import { projetos } from '@/data/project';
 
 Modal.setAppElement('#root');
 
@@ -25,32 +23,6 @@ export function Projects() {
       once: false,
     });
   }, []);
-
-  const projetos = [
-    {
-      image: coffeeDelivery,
-      description:
-        'E-commerce que simula um delivery de café, utilizando de hooks como useContext, useState e useEffect e consumindo APIs.',
-      languages: ['React', 'TypeScript', 'TailWindCss'],
-      repository: 'https://github.com/Murillou/coffee-delivery',
-      deploy: 'https://coffee-delivery.murillou.dev/',
-    },
-    {
-      image: nextShop,
-      description:
-        'Simulação de e-commerce desenvolvido com Next.js e Stripe para integração de pagamentos.',
-      languages: ['Next.js', 'TypeScript', 'Stitches'],
-      repository: 'https://github.com/Murillou/next-shop',
-      deploy: 'https://next-shop.murillou.dev/',
-    },
-    {
-      image: todoList,
-      description: 'Lista de tarefas para gestão de obrigações.',
-      languages: ['React', 'TypeScript'],
-      repository: 'https://github.com/Murillou/todo-list',
-      deploy: 'https://todo-list.murillou.dev/',
-    },
-  ];
 
   function openModal(image: string) {
     setSelectedImage(image);

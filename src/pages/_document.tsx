ab1e4578c54f6996cc4dcf23e129d43c5e6d8421
Murillou <murillovinicius13@hotmail.com>
2025-02-16 20:29:57 -0300

    chore: adiciona título na página junto do ícone

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head />
+      <Head>
+        <link rel="icon" href="/icon-dev.png" />
+        <title>Dev Murillou</title>
+      </Head>{' '}
       <body id="root" className="antialiased">
         <Main />
         <NextScript />

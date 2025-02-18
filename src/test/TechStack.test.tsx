483bea2aea6220b1bb0c752b07fa2c7f2064ce57
Murillou <murillovinicius13@hotmail.com>
2025-02-18 10:45:13 -0300

    test: implementa testes unitários em TechStack

---
+++ src/test/TechStack.test.tsx
@@ -0,0 +1,20 @@
+import { TechStack } from '@/components/TechStack/TechStack';
+import { techStack } from '@/data/teckStack';
+import { render, screen } from '@testing-library/react';
+
+describe('Tech Stack componente', () => {
+  it('deve renderizar corretamente o "Tech Stack"', () => {
+    render(<TechStack />);
+
+    expect(screen.getByText('Tech Stack')).toBeInTheDocument();
+  });
+
+  it('deve renderizar o nome e icones da tech', () => {
+    render(<TechStack />);
+
+    techStack.forEach(tech => {
+      const techName = screen.getByText(tech.name);
+      expect(techName).toBeInTheDocument();
+    });
+  });
+});
